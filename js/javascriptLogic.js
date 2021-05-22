eb4c9bb60099a8fcbb066e331d21ed0c41e94768
Cesar Choquehuanca <cesarchoqueskater@gmail.com>
2021-05-21 23:05:33 -0500

    Rediseño Interfaz App Completo

--- js/javascriptLogic.js
+++ js/javascriptLogic.js
@@ -3,14 +3,16 @@ var Papel = document.getElementById("paper-choose");
 var Tijera = document.getElementById("scissors-choose");
 var btn_newplay = document.getElementById("button-click");
 var resultCompare = document.getElementById("result");
-var btn_choose = document.getElementById("selection");
+var btn_choose = document.getElementsByClassName("image-choose");
 var Header_information = document.getElementById("selection-information-header");
 
 //Obtiene la imagen de la opcion seleccionada para mostrarla
 function selectOption(optionChoose) {
-    var imgOption = "img/" + optionChoose + ".png";
+    var imgOption = "img/" + optionChoose + "-icon.svg";
     // console.log(" Img es : " + imgOption);
     document.getElementById("option-choose-play").src = imgOption;
+    document.getElementById("option-left-box").classList.add("background-color-new");
+    document.getElementById("option-right-box").classList.add("background-color-new");
     btn_newplay.style.display = "block";
 }
 
@@ -19,7 +21,7 @@ function randomObject() {
     var objects = ['Piedra', 'Papel', 'Tijera'];
     var randomObject = objects[Math.floor(Math.random() * objects.length)];
     // console.log(objects.length);
-    document.getElementById("option-choose-computer").src = ("img/" + randomObject + ".png");
+    document.getElementById("option-choose-computer").src = ("img/" + randomObject + "-icon.svg");
     // console.log(randomObject);
     return randomObject;
 }
@@ -34,24 +36,27 @@ function compareFunction(compareSelect) {
             resultCompare.innerHTML = "¡¡Esto es un empate!!";
             break;
         case (compareSelect == "Piedra" && varRandomObject == "Tijera"):
-            resultCompare.innerHTML = "¡¡Ganaste!! con " + compareSelect + " vs " + varRandomObject;
+            resultCompare.innerHTML = "¡¡Ganaste!! <br> Con " + compareSelect + " vs " + varRandomObject;
             break;
         case (compareSelect == "Papel" && varRandomObject == "Piedra"):
-            resultCompare.innerHTML = "¡¡Ganaste!! con " + compareSelect + " vs " + varRandomObject;
+            resultCompare.innerHTML = "¡¡Ganaste!! <br> Con " + compareSelect + " vs " + varRandomObject;
             break;
         case (compareSelect == "Tijera" && varRandomObject == "Papel"):
-            resultCompare.innerHTML = "¡¡Ganaste!! con " + compareSelect + " vs " + varRandomObject;
+            resultCompare.innerHTML = "¡¡Ganaste!! <br> Con " + compareSelect + " vs " + varRandomObject;
             break;
         default:
             resultCompare.innerHTML = "Perdiste :(";
     }
 
     //Bloque los botonos para escoger, para poder ser activados en un evento mas adelante
-    btn_choose.classList.add("img-disabled");
+    // btn_choose.classList.add("img-disabled");
+    Piedra.classList.add("img-disabled");
+    Papel.classList.add("img-disabled");
+    Tijera.classList.add("img-disabled");
     Piedra.disabled = true;
     Papel.disabled = true;
     Tijera.disabled = true;
-    Header_information.style.color = "grey";
+    Header_information.style.color = "var(--text-grey)";
 }
 
 Piedra.addEventListener("click", function() {
@@ -70,14 +75,17 @@ Tijera.addEventListener("click", function() {
 //Boton para permitir volver a jugar
 btn_newplay.addEventListener("click", function() {
     var defaultImage = "img/ImageBackground";
-    console.log("vae");
-    document.getElementById("option-choose-play").src = defaultImage + "-User.png";
-    document.getElementById("option-choose-computer").src = defaultImage + "-Computer.png";
+    document.getElementById("option-choose-play").src = defaultImage + "-User.svg";
+    document.getElementById("option-choose-computer").src = defaultImage + "-Computer.svg";
     btn_newplay.style.display = "none";
-    btn_choose.classList.remove("img-disabled");
+    Piedra.classList.remove("img-disabled");
+    Papel.classList.remove("img-disabled");
+    Tijera.classList.remove("img-disabled");
     Piedra.disabled = false;
     Papel.disabled = false;
     Tijera.disabled = false;
+    document.getElementById("option-left-box").classList.remove("background-color-new");
+    document.getElementById("option-right-box").classList.remove("background-color-new");
     Header_information.style.color = "var(--p-text)";
     resultCompare.innerHTML = "";
 });
